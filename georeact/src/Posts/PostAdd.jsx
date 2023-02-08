e267e42c6ba2161341f970a02c2bc16f7b123932
hector-artiso <hearca@fp.insjoaquimmir.cat>
2023-02-08 19:24:25 +0100

    proba

--- georeact/src/Posts/PostAdd.jsx
+++ georeact/src/Posts/PostAdd.jsx
@@ -3,32 +3,41 @@ import { UserContext } from "../userContext";
 
 function PostAdd() {
   let { authToken,setAuthToken } = useContext(UserContext);
+  var [ formulari, setFormulari ] = useState({});
 
-      useEffect(() => {
-
-        navigator.geolocation.getCurrentPosition( (pos )=> {
+    const handleChange = (e) => {
+      e.preventDefault();
+      if (e.target.name==="upload")
+        {
+          console.log(e.target.files[0].name)
           setFormulari({
             ...formulari,
-            latitude :  pos.coords.latitude,
-            longitude: pos.coords.longitude,
-            visibility : 1
+            [e.target.name] : e.target.files[0]
           })
+        }
+      else {
+            setFormulari({
+              ...formulari,
+              [e.target.name] : e.target.value
+            })
+        };
+    }
       
-        });
-      }, [])
       const addPost = async(e) => {
-        e.PreventDefault();
-        let { body, latitude , longitude, visibility };
+        e.preventDefault();
+
+        let { body, latitude , longitude, visibility=1, upload }  = formulari;
         console.log(formulari);
-        const formdata = new FormData();
-        formdata.append('name', name);
+        const formdata = new FormData();        
         formdata.append('body', body);
+        formdata.append('upload', upload);
         formdata.append('latitude', latitude);
         formdata.append('longitude', longitude);
         formdata.append('visibility', visibility);
+        console.log(formulari)
 
         try{
-          const data = await fetch('https://backend.insjoaquimmir.cat/api/places', {
+          const data = await fetch('https://backend.insjoaquimmir.cat/api/posts', {
             headers: {
               'Accept': 'application/json',
               'Authorization': 'Bearer ' + authToken
@@ -39,21 +48,71 @@ function PostAdd() {
           const resposta = await data.json();
           if (resposta.success === true){
             console.log(resposta);
-            formaddpost.reset();
+            setFormulari({})
             alert ('Post successfully uploaded');
           } else{
             alert('There was an error uploading the post');
-            console.log(formulari);
+            console.log(resposta);
+            
           }
         }catch{
           console.log('Error')
         }
       }
+      useEffect(() => {
+
+        navigator.geolocation.getCurrentPosition( (pos )=> {
+          setFormulari({
+            ...formulari,
+            latitude :  pos.coords.latitude,
+            longitude: pos.coords.longitude,
+            
+          })
+      
+        });
+      }, [])
     return (
     <>
       <div className="containerAdd">
-        
-      </div>
+      <form id="formAddPost"className="addPost">
+            <div className="title"><h3>Add New Post</h3></div>
+
+            <div>
+              <input type="text"placeholder="Body" id="body" name="body" onChange={handleChange}/>
+            </div>
+
+            <div>
+              <input type="number" placeholder="Latitude" id="latitude" name="latitude" value = { formulari.latitude } onChange={handleChange}/>
+            </div>
+
+            <div>
+              <input type="number"placeholder="Longitude" id="longitude" name="longitude" value = { formulari.longitude } onChange={handleChange}/>
+            </div>
+              <div>
+                <input type="radio" id="visibility" name="visibility" value="1" checked onChange={handleChange}/>
+                <label htmlFor="public">Public</label>
+              </div>
+              <div>
+                <input type="radio" id="visibility" name="visibility" value="2" onChange={handleChange}/>
+                <label htmlFor="private">Contacts</label>
+              </div>
+              <div>
+                <input type="radio" id="visibility" name="visibility" value="3" onChange={handleChange}/>
+                <label htmlFor="private">Private</label>
+              </div>
+            <div>
+              <input type="file" placeholder="Upload" id="upload" name="upload" onChange={handleChange}/>
+            </div>
+           
+            <button className="addPlaceButton"
+              onClick={addPost}>
+              Submit
+            </button>		
+
+          </form>
+      </div>		
+   
+     
     </>
   )
 }
