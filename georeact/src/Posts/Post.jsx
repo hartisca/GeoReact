7d2752161503a36811cab718afa3bd66f4e18ede
hector-artiso <hearca@fp.insjoaquimmir.cat>
2023-02-09 18:42:13 +0100

    Fet el Post show

--- georeact/src/Posts/Post.jsx
+++ georeact/src/Posts/Post.jsx
@@ -8,54 +8,51 @@ import { FcLike } from 'react-icons/fc';
 function Post() {
   let { id } = useParams();
   let { userEmail, setUserEmail, authToken, setAuthToken } = useContext(UserContext);
-  let [ isLoading, setIsLoading ] = useState (true);
-  let [ post, setPost ] = useState ({});
+  let [isLoading, setIsLoading] = useState(true)
+  let [post, setPost] = useState({});
 
-  const getPost = async(e) => {
-    try{
-      const data = await fetch('https://backend.insjoaquimmir.cat/api/posts/' + id, {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          'Authorization': 'Bearer '  + authToken,
-        },
-        method: "GET"
-      })
-      const resposta = await data.json();
-      if (resposta.success === true){
-        setPost(resposta.data);
-        console.log(resposta);
-        console.log(isLoading);
-        setIsLoading(false);
-      } else {
-        alert ('Error en la resposta');
-      }
-    } catch{
-      console.log('error');
-    }
+const getPost = async(e) => {
+  try{
+    
+      const data = await fetch("https://backend.insjoaquimmir.cat/api/posts/" + id, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        'Authorization': 'Bearer '  + authToken,
+      },
+      method: "GET"
+    })
+    const resposta = await data.json();
+    if (resposta.success === true) setPost(resposta.data), console.log(resposta), setIsLoading(false);
+    
+    else (console.log(resposta));
 
-    useEffect(() => {
-      getPost();
-    }, [])
+    }catch{
+      console.log("Error");
+      alert("catch");  
+    }    
+}
 
-  }
-  return (
-    <>
-      {(isLoading == true) ? <div>Carregant dades...</div> :
-      <div>
-      <img className="imgGrid" src={"https://backend.insjoaquimmir.cat/storage/" + post.file.filepath } alt={ post.file.id } width="300"/>
-        <p>Autor: {post.author.name}</p>
-        <p>Latitud: {post.latitude}</p>
-        <p>Longitud: {post.longitude}</p>
-        <div className='InfoPost'>
-            <p>Descripció: </p>
-            {post.body}     
-        </div>
-        <FcLike />{post.likes_count}
-      </div>
-    }
-    </>
-  )
+  useEffect(() => { getPost(); }, []);
+return (
+
+  <>{(isLoading == true) ? <div>Carregant dades...</div> :
+  <div>
+  <img className="imgGrid" src={"https://backend.insjoaquimmir.cat/storage/" + post.file.filepath } alt={ post.file.id } width="300"/>
+    <p>Autor: {post.author.name}</p>
+    <p>Latitud: {post.latitude}</p>
+    <p>Longitud: {post.longitude}</p>
+    <div className='InfoPost'>
+        <p>Descripció: </p>
+        {post.body}    
+    </div>
+    <FcLike />{post.likes_count}
+  </div>
+}
+
+  </> 
+
+)
 }
 
 export default Post
